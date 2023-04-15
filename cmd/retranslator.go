77f46422c1fe86358caa7e39f101093a6af62da2
Alejandro <abergasov@gmail.com>
2023-04-15 16:21:11 +0300

    deps missed

--- cmd/retranslator.go
+++ cmd/retranslator.go
@@ -7,9 +7,11 @@ import (
 	"syscall"
 
 	"github.com/abergasov/retranslator/pkg/logger"
+	"github.com/abergasov/retranslator/pkg/service/counter"
 	"github.com/abergasov/retranslator/pkg/service/requester/executor"
 	"github.com/abergasov/retranslator/pkg/service/requester/orchestrator"
 	"github.com/abergasov/retranslator/pkg/service/retranslator/client"
+	"github.com/abergasov/retranslator/pkg/storage/database"
 	"go.uber.org/zap"
 )
 
@@ -17,6 +19,7 @@ var (
 	appHash = os.Getenv("GIT_HASH")
 	//targetHost = "localhost:48292"
 	targetHost = "165.232.118.116:48292"
+	dbPath     = "storage.db"
 )
 
 func main() {
@@ -24,11 +27,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("unable to create logger: %s", err)
 	}
+	dbConnect, err := database.InitDBConnect(dbPath)
+	if err != nil {
+		appLog.Fatal("unable to connect to db", err)
+	}
+	defer dbConnect.Close()
+	requestCounter := counter.NewService(appLog.With(zap.String("service", "counter")), dbConnect)
 
 	curler := executor.NewService(appLog.With(zap.String("service", "executor")))
 	orchestra := orchestrator.NewService(appLog.With(zap.String("service", "orchestrator")), curler)
 
-	relay := client.NewRelay(appLog.With(zap.String("service", "relay")), targetHost, orchestra)
+	relay := client.NewRelay(appLog.With(zap.String("service", "relay")), targetHost, orchestra, requestCounter)
 	defer relay.Stop()
 
 	relay.Start()
